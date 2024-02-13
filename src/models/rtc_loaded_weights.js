fc9b78b2d07ae311ca096ac71b59073c2160ac53
nduwa <nduwapaci12@gmail.com>
2024-02-13 13:50:31 +0200

    new models

---
+++ src/models/rtc_loaded_weights.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require('sequelize');
+import sequelize from '../database/connectDb';
+
+const loaded_weights = sequelize.define('loaded_weights',{
+    id:{
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    created_at:{
+        type: DataTypes.DATE,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull:false
+    },
+    created_by:{
+        type: DataTypes.INTEGER,
+        allowNull:false
+    },
+    rtc_delivery_reports_id:{
+        type: DataTypes.INTEGER,
+        allowNull:false
+    },
+    rtc_transaction_id:{
+        type: DataTypes.INTEGER,
+        allowNull:false
+    },
+    weight_loaded:{
+        type: DataTypes.DOUBLE,
+        allowNull:false
+    },
+    weight_left:{
+        type: DataTypes.DOUBLE,
+        allowNull:false
+    },
+    total_weight_left:{
+        type: DataTypes.DOUBLE,
+        allowNull:false
+    },
+    contribution_sent_at:{
+        type: DataTypes.DATE,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull:false
+    }
+});
+module.exports = loaded_weights;
