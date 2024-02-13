b3f1578aebf76ab3d58af432acd0f4395d8d9994
Kwizera Pacifique <kwizerapacifique19@gmail.com>
2024-02-13 11:22:30 +0200

    bug fixing on user registration

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,74 +1,81 @@
-import Users from '../models/rtc_users'
-import userValidationSchema from '../validations/userValidations';
-import bcrypt from 'bcrypt'
+import { Op } from "sequelize";
+import Users from "../models/rtc_users";
+import userValidationSchema from "../validations/userValidations";
+import bcrypt from "bcrypt";
 
 class UserController {
-//user creation/signup/adding or registering a user
-    static async createUser(req, res) {
-        try {
-        //   user validation
-    
-          const { error } = userValidationSchema.validate(req.body);
-    
-          if (error)
-            return res.status(400).json({
-              status: "fail",
-              validationError: error.details[0].message,
-            });
-    
-          const checkduplicatedEmail = await Users.findOne({ Email: req.body.Email });
-          if (checkduplicatedEmail) {
-            return res.status(400).json({
-              status: "fail",
-              message: `User with ${req.body.Email} already exist`,
-            });
-          }
-    
-          const hashedPassword = await bcrypt.hash(req.body.password, 10);
-         
-    
-          const user = new Users({
-            status: req.body.status,
-            __kp_User: req.body.__kp_User,
-            _kf_Location: req.body._kf_Location,
-            Name_User: req.body.Name_User,
-            Role: req.body.Role,
-            z_recCreateAccountName: req.body.z_recCreateAccountName,
-            z_recCreateTimestamp: req.body.z_recCreateTimestamp,
-            z_recModifyAccountName: req.body. z_recModifyAccountName,
-            z_recModifyTimestamp: req.body.z_recModifyTimestamp,
-            Phone: req.body.Phone,
-            Phone_Airtime: req.body.Phone_Airtime,
-            password: hashedPassword,
-            devicename: req.body. devicename,
-            last_update_at: req.body.last_update_at,
-            password: hashedPassword,
-          
-          });
-          await user.save();
-     
-          res.status(200).json({
-            status: "success",
-            message: "User created successfully",
-            data: user,
-          });
-         
-        } catch (error) {
-          res.status(500).json({
-            status: "Fail",
-            message: error.message,
-          });
-          console.log(error);
-        }
+  //user creation/signup/adding or registering a user
+  static async createUser(req, res) {
+    try {
+      //   user validation
+
+      // console.log(req.body);
+      const { error } = userValidationSchema.validate(req.body);
+
+      if (error)
+        return res.status(400).json({
+          status: "fail",
+          validationError: error.details[0].message,
+        });
+
+      const checkduplicatedEmail = await Users.findOne({
+        where: {
+          [Op.or]: [{ Email: req.body.email }, { Phone: req.body.Phone }],
+        },
+      });
+      // console.log(checkduplicatedEmail);
+      if (checkduplicatedEmail) {
+        return res.status(400).json({
+          status: "fail",
+          message: `User with ${req.body.Email} already exist`,
+        });
       }
 
-      //get all users from users table
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+      const user = new Users({
+        status: 0,
+        __kp_User: req.body.__kp_User,
+        _kf_Location: req.body._kf_Location,
+        Name_Full: req.body.Name_Full,
+        Name_User: req.body.Name_User,
+        Role: req.body.Role,
+        z_recCreateAccountName: req.body.z_recCreateAccountName,
+        z_recCreateTimestamp: req.body.z_recCreateTimestamp,
+        z_recModifyAccountName: req.body.z_recModifyAccountName,
+        z_recModifyTimestamp: req.body.z_recModifyTimestamp,
+        Phone: req.body.Phone,
+        Phone_Airtime: req.body.Phone_Airtime,
+        Email: req.body.email,
+        devicename: req.body.devicename,
+        last_update_at: req.body.last_update_at,
+        password: hashedPassword,
+      });
+      await user.save();
+
+      res.status(200).json({
+        status: "success",
+        message: "User created successfully",
+        data: user,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "Fail",
+        message: error.message,
+      });
+      console.log(error);
+    }
+  }
+
+  //get all users from users table
   static async getAllUsers(req, res) {
     try {
       const users = await Users.findAll();
       console.log("users", users.length);
       if (!users || users.length === 0) {
-        return res.status(404).json({ status: "fail", message: "No users found" });
+        return res
+          .status(404)
+          .json({ status: "fail", message: "No users found" });
       }
       return res.status(200).json({ status: "success", data: users });
     } catch (error) {
@@ -76,12 +83,11 @@ class UserController {
     }
   }
 
-
   //logging in with username and password
   static async login(req, res) {
     try {
-      const user = await Users.findOne({  Name_User: req.body. Name_User });
-      console.log("user name",user)
+      const user = await Users.findOne({ Name_User: req.body.Name_User });
+      console.log("user name", user);
       if (!user) {
         return res.status(400).json({
           status: "fail",
@@ -106,14 +112,17 @@ class UserController {
 
       // create token
 
-
-      
-      const token = Jwt.sign({ user:{
-        Name_User:user.Name_User,
-
-      } }, process.env.JWT_SECRET, {
-        expiresIn: "2d",
-      });
+      const token = Jwt.sign(
+        {
+          user: {
+            Name_User: user.Name_User,
+          },
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "2d",
+        }
+      );
 
       res.status(200).json({
         status: "success",
@@ -128,7 +137,6 @@ class UserController {
       });
     }
   }
-
 }
 
 export default UserController;
--- src/validations/userValidations.js
+++ src/validations/userValidations.js
@@ -1,10 +1,10 @@
 import Joi from "joi";
 const userValidationSchema = Joi.object({
-    Name_Full: Joi.string()
+  Name_Full: Joi.string()
     .min(3)
     .max(20)
     .required()
-    .pattern(/^[A-Za-z]+$/)
+    .pattern(/^[A-Za-z ]+$/)
     .messages({
       "string.pattern.base":
         "your name can not contain numbers and special characters",
@@ -14,7 +14,7 @@ const userValidationSchema = Joi.object({
     }),
 
   Name_User: Joi.string()
-    .min(7)
+    .min(4)
     .max(20)
     .required()
     .pattern(/^[A-Za-z]+$/)
@@ -26,21 +26,35 @@ const userValidationSchema = Joi.object({
       "string.min": "The last name can not be less than 10 characters long",
     }),
 
-  Email: Joi.string().trim().email().required().messages({
+  email: Joi.string().trim().email().required().messages({
     "string.email": "Invalid email type",
     "string.empty": "Email field can not be empty",
     "any.required": "Email is required",
   }),
-  password: Joi.string()
-    .max(16)
+  password: Joi.string().min(8).max(16).required().messages({
+    "string.pattern.base":
+      "The password should have at least one capital letter and a number",
+    "string.empty": "The password field can not be empty",
+    "string.required": "The password field is required",
+    "string.max": "Password can not exceed 16 characters",
+  }),
+  __kp_User: Joi.string().required(),
+  _kf_Location: Joi.string().required(),
+  Name_User: Joi.string()
     .required()
-    .regex(/^(?=(.*[A-Z]){1,})(?=(.*[a-z]){1,})(?=(.*[0-9]){1,}).{5,}$/)
-    .messages({
-      "string.pattern.base":
-        "The password should have at least one capital letter and a number",
-      "string.empty": "The password field can not be empty",
-      "string.required": "The password field is required",
-      "string.max": "Password can not exceed 16 characters",
-    }),
+    .pattern(/^[A-Za-z]+$/),
+  Role: Joi.string()
+    .required()
+    .pattern(/^[A-Za-z]+$/),
+  z_recCreateAccountName: Joi.string().required(),
+  z_recCreateTimestamp: Joi.string().required(),
+  z_recModifyAccountName: Joi.string().required(),
+  z_recModifyTimestamp: Joi.string().required(),
+  Phone: Joi.string().required(),
+  Phone_Airtime: Joi.string().required(),
+  devicename: Joi.string()
+    .required()
+    .pattern(/^[A-Za-z]+$/),
+  last_update_at: Joi.string().required(),
 });
 export default userValidationSchema;
