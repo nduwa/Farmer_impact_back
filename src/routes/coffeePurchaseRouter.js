8e275f3f56ea85834a8b0a9c0812c689c4508f35
Luciefifi <niyomutonilucie@gmail.com>
2024-03-13 16:38:56 +0200

    cherry lot details

--- src/routes/coffeePurchaseRouter.js
+++ src/routes/coffeePurchaseRouter.js
@@ -1,17 +1,45 @@
-import express from 'express'
-import coffeePuchase from '../controllers/coffeePurchaseController'
-import verifyToken from '../middlewares/auth'
+import express from "express";
+import coffeePuchase from "../controllers/coffeePurchaseController";
+import verifyToken from "../middlewares/auth";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/dailyJournal', verifyToken, coffeePuchase.getSCDailyJournals)
-router.get('/dailyJournal/:journalId', verifyToken, coffeePuchase.getSCDailyJournalsByJournalId)
-router.get('/dailyJournal/transaction/:id', verifyToken, coffeePuchase.getTransactionById)
-router.put('/dailyJournal/transaction/:id', verifyToken, coffeePuchase.removeTransaction)
-router.put('/dailyJournal/transaction/update/:id', verifyToken, coffeePuchase.updateTransaction)
-// router.post('/dailyJournal/fees', verifyToken, coffeePuchase.addFees)
-router.post('/dailyJournal/commissions', verifyToken, coffeePuchase.addCommissions)
-router.get('/dailyJournal/daylots', coffeePuchase.getAllDayLot)
-router.put('/dailyJournal/journal/approve/:journalId', verifyToken, coffeePuchase.approveJournal)
+router.get("/dailyJournal", verifyToken, coffeePuchase.getSCDailyJournals);
+router.get(
+  "/dailyJournal/:journalId",
+  verifyToken,
+  coffeePuchase.getSCDailyJournalsByJournalId
+);
+router.get(
+  "/dailyJournal/transaction/:id",
+  verifyToken,
+  coffeePuchase.getTransactionById
+);
+router.put(
+  "/dailyJournal/transaction/:id",
+  verifyToken,
+  coffeePuchase.removeTransaction
+);
+router.put(
+  "/dailyJournal/transaction/update/:id",
+  verifyToken,
+  coffeePuchase.updateTransaction
+);
+router.post(
+  "/dailyJournal/commissions",
+  verifyToken,
+  coffeePuchase.addCommissions
+);
+router.get("/dailyJournal/daylots", coffeePuchase.getAllDayLot);
+router.put(
+  "/dailyJournal/journal/approve/:journalId",
+  verifyToken,
+  coffeePuchase.approveJournal
+);
+router.get(
+  "/dailyJournal/journal/:cherryLotId",
+  verifyToken,
+  coffeePuchase.getWSCDailyJournalsByDate
+);
 
-export default router
+export default router;
