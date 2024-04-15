a7a586317e7a86b47e38a51c0c3f48dfe9aef405
Luciefifi <niyomutonilucie@gmail.com>
2024-04-15 14:12:30 +0200

    parchment stock and adjustment

--- src/routes/parchmentRoute.js
+++ src/routes/parchmentRoute.js
@@ -6,5 +6,6 @@ const parchmentRoutes = express.Router()
 parchmentRoutes.post('/assign',ParchmentController.assignNewParchment);
 parchmentRoutes.post('/parchGrade',verifyToken,ParchmentController.assignParchmentGrade)
 parchmentRoutes.get('/allAssignedParchments', ParchmentController.getAllAssignedParchments)
+parchmentRoutes.put('/adjust', ParchmentController.adjustParchment)
 
 export default parchmentRoutes
