2fd42095f0146958b58decb67887cf785885f6cb
Luciefifi <niyomutonilucie@gmail.com>
2024-05-06 15:56:27 +0200

    delivery processing

---
+++ src/routes/deliveryProcessingRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import DeliveryProcesingController from "../controllers/deliveryProcessingController";
+const deliveryProcessingRouter = express.Router()
+
+deliveryProcessingRouter.get('/loadedweight/:id', DeliveryProcesingController.getLoadedWeightById)
+deliveryProcessingRouter.get('/contributions_by_id/:id', DeliveryProcesingController.getProcessedContributionById)
+deliveryProcessingRouter.get('/contributions/', DeliveryProcesingController.getProcessedContributions)
+deliveryProcessingRouter.post('/start_processing',DeliveryProcesingController.startProcessing)
+export default deliveryProcessingRouter
