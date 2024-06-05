783d7008d7ecf7b1885e02e872b3f4fb8737f705
Luciefifi <niyomutonilucie@gmail.com>
2024-06-05 15:08:57 +0200

    rebased main

--- src/routes/parchmentTransportRoute.js
+++ src/routes/parchmentTransportRoute.js
@@ -1,32 +1,14 @@
-import express from "express";
-import ParchmentTransportController from "../controllers/parchmentTransportController";
-import verifyToken from "../middlewares/auth";
-const parchmentTransportRoutes = express.Router();
+import express from 'express'
+import ParchmentTransportController from '../controllers/parchmentTransportController';
+import deliveryReportController from '../controllers/deliveryReportController';
+import verifyToken from '../middlewares/auth';
+const parchmentTransportRoutes = express.Router()
 
-parchmentTransportRoutes.post(
-  "/deliver",
-  verifyToken,
-  ParchmentTransportController.deliverParchment
-);
-parchmentTransportRoutes.get(
-  "/reports",
-  ParchmentTransportController.getDeliveryReports
-);
-parchmentTransportRoutes.get(
-  "/reportLots",
-  ParchmentTransportController.getDeliveryReportLots
-);
-parchmentTransportRoutes.get(
-  "/lot_by_id/:id",
-  ParchmentTransportController.getDeliveryReportLotById
-);
-parchmentTransportRoutes.get(
-  "/report_by_id/:id",
-  ParchmentTransportController.getDeliveryReportById
-);
-parchmentTransportRoutes.put(
-  "/update/:id",
-  verifyToken,
-  ParchmentTransportController.updateDeliveryReport
-);
-export default parchmentTransportRoutes;
+parchmentTransportRoutes.post('/deliver',verifyToken,ParchmentTransportController.deliverParchment);
+parchmentTransportRoutes.get('/reports',ParchmentTransportController.getDeliveryReports);
+parchmentTransportRoutes.get('/reportLots',ParchmentTransportController.getDeliveryReportLots);
+parchmentTransportRoutes.get('/lot_by_id/:id',ParchmentTransportController.getDeliveryReportLotById);
+parchmentTransportRoutes.get('/report_by_id/:id',ParchmentTransportController.getDeliveryReportById);
+parchmentTransportRoutes.put('/update/:id',verifyToken,ParchmentTransportController.updateDeliveryReport);
+parchmentTransportRoutes.post('/deliverParchment/',verifyToken,deliveryReportController.report_delivery);
+export default parchmentTransportRoutes
