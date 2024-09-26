f542c4509bc15dbd7f38fdd0efac911d283cf862
Kwizera Pacifique <kwizerapacifique19@gmail.com>
2024-09-26 09:45:12 +0200

    more seeds for census survey

--- src/seeders/20240920175944-pest-diseases.js
+++ src/seeders/20240920175944-pest-diseases.js
@@ -18,6 +18,20 @@ module.exports = {
         name: "Antestia",
         level: "1/3 of the farm",
       },
+      {
+        created_at: new Date(),
+        __kp_pests_diseases: "B82CA864-1A3E-1782-1006-71C4A4D35D23",
+        _kf_trees_survey: "4860FB17-4F28-4342-F815-2CD4E6451932",
+        name: "Leaf rust",
+        level: "More than half of the farm",
+      },
+      {
+        created_at: new Date(),
+        __kp_pests_diseases: "B82CA864-1A3E-1782-1006-71C4A4D35D23",
+        _kf_trees_survey: "4860FB17-4F28-4342-F815-2CD4E6451932",
+        name: "Antestia",
+        level: "1/3 of the farm",
+      },
     ]);
   },
 
