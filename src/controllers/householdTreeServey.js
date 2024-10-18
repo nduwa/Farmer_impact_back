aca0d8e522deef8d92b4d09c4abba030af9f85a9
Luciefifi <niyomutonilucie@gmail.com>
2024-10-18 11:10:04 +0200

    fixing census survey household trees

--- src/controllers/householdTreeServey.js
+++ src/controllers/householdTreeServey.js
@@ -133,146 +133,146 @@ class HouseholdTreeServey {
       });
     }
   }
-  static async insertRecord() {
-    try {
-      const newRecord = await TreeServey.create(
-        {
-          created_at: new Date(),
-          __kp_house_trees: "DFF32029-405F-BD06-F24C-524157B66281",
-          _kf_Staff: "644072CF-DE4C-1F4F-A6E5-373A2AF0D071",
-          _kf_User: "83A06EBA-E5D7-0B4A-99E6-9C5674B83A7B",
-          full_name: "Nubuhoro Emmanuel",
-          _kf_Station: "F9642FF7-694C-4B99-A5B4-0FA1028C4AEC",
-          _kf_Supplier: "07463891-1754-4912-B22B-FA01835012D8",
-          CW_Name: "Muhura",
-          Group_ID: "MM004",
-          farmer_ID: "F2315A",
-          farmer_name: "Sibomana Theoneste",
-          national_ID: "11995856665656",
-          Phone: "1234567890",
-          Year_Birth: 1980,
-          Gender: "Male",
-          child_year_1_20: 2,
-          child_year_20_30: 3,
-          source_income: "coffee",
-          Trees: 10,
-          Trees_Producing: 5,
-          Trees_year_less_10: 3,
-          Trees_year_10_20: 4,
-          Trees_year_greater_20: 7,
-          coffee_plot: 2.5,
-          last_season: 2023,
-          last_season_production: 1000,
-          current_season: 2024,
-          current_season_production: 1200,
-          nitrogen: "8",
-          natural_shade: "12",
-          shade_trees: "20",
-          other_crops_coffee_farm: "Value",
-          other_crops_farm: "Value",
-          longitude: 34.123456,
-          latitude: -0.123456,
-          seedling_last_3_year: 2021,
-          received_tree_3_y: 20,
-          seedling_last_2_year: 2022,
-          received_tree_2_y: 15,
-          seedling_last_year: 2023,
-          received_tree_l_y: 10,
-          rejuvenation_last_year: 2023,
-          rejuvenated_l_tree: 7,
-          rejuvenation_current_year: 2024,
-          rejuvenated_c_tree: 12,
-          status: "new",
-          Mulching: "Value",
-          compost_heap: "Value",
-          pruning: "Value",
-          trenches: "Value",
-          rejuvenation: "Value",
-          weed_control: "Value",
-          traps: "Value",
-          contour_grass: "Value",
-          inorganic_waste: "Value",
-          leaf_rust: "Value",
-          berry_borer: "Value",
-          berry_desease: "Value",
-          white_stem_borer: "Value",
-          scare_mealy_bug: "Value",
-          antestia: "Value",
-          leaf_miner: "Value",
-        },
-        {
-          created_at: new Date(),
-          __kp_house_trees: "DFF32029-405F-BD06-F24C-524157B66281",
-          _kf_Staff: "644072CF-DE4C-1F4F-A6E5-373A2AF0D071",
-          _kf_User: "83A06EBA-E5D7-0B4A-99E6-9C5674B83A7B",
-          full_name: "Nubuhoro Emmanuel",
-          _kf_Station: "F9642FF7-694C-4B99-A5B4-0FA1028C4AEC",
-          _kf_Supplier: "07463891-1754-4912-B22B-FA01835012D8",
-          CW_Name: "Muhura",
-          Group_ID: "MM004",
-          farmer_ID: "F2315A",
-          farmer_name: "Sibomana Theoneste",
-          national_ID: "11995856665656",
-          Phone: "1234567890",
-          Year_Birth: 1980,
-          Gender: "Male",
-          child_year_1_20: 2,
-          child_year_20_30: 3,
-          source_income: "coffee",
-          Trees: 10,
-          Trees_Producing: 5,
-          Trees_year_less_10: 3,
-          Trees_year_10_20: 4,
-          Trees_year_greater_20: 7,
-          coffee_plot: 2.5,
-          last_season: 2023,
-          last_season_production: 1000,
-          current_season: 2024,
-          current_season_production: 1200,
-          nitrogen: "8",
-          natural_shade: "12",
-          shade_trees: "20",
-          other_crops_coffee_farm: "Value",
-          other_crops_farm: "Value",
-          longitude: 34.123456,
-          latitude: -0.123456,
-          seedling_last_3_year: 2021,
-          received_tree_3_y: 20,
-          seedling_last_2_year: 2022,
-          received_tree_2_y: 15,
-          seedling_last_year: 2023,
-          received_tree_l_y: 10,
-          rejuvenation_last_year: 2023,
-          rejuvenated_l_tree: 7,
-          rejuvenation_current_year: 2024,
-          rejuvenated_c_tree: 12,
-          status: "new",
-          Mulching: "Value",
-          compost_heap: "Value",
-          pruning: "Value",
-          trenches: "Value",
-          rejuvenation: "Value",
-          weed_control: "Value",
-          traps: "Value",
-          contour_grass: "Value",
-          inorganic_waste: "Value",
-          leaf_rust: "Value",
-          berry_borer: "Value",
-          berry_desease: "Value",
-          white_stem_borer: "Value",
-          scare_mealy_bug: "Value",
-          antestia: "Value",
-          leaf_miner: "Value",
-        }
-      );
+  // static async insertRecord() {
+  //   try {
+  //     const newRecord = await TreeServey.create(
+  //       {
+  //         created_at: new Date(),
+  //         __kp_house_trees: "DFF32029-405F-BD06-F24C-524157B66281",
+  //         _kf_Staff: "644072CF-DE4C-1F4F-A6E5-373A2AF0D071",
+  //         _kf_User: "83A06EBA-E5D7-0B4A-99E6-9C5674B83A7B",
+  //         full_name: "Nubuhoro Emmanuel",
+  //         _kf_Station: "F9642FF7-694C-4B99-A5B4-0FA1028C4AEC",
+  //         _kf_Supplier: "07463891-1754-4912-B22B-FA01835012D8",
+  //         CW_Name: "Muhura",
+  //         Group_ID: "MM004",
+  //         farmer_ID: "F2315A",
+  //         farmer_name: "Sibomana Theoneste",
+  //         national_ID: "11995856665656",
+  //         Phone: "1234567890",
+  //         Year_Birth: 1980,
+  //         Gender: "Male",
+  //         child_year_1_20: 2,
+  //         child_year_20_30: 3,
+  //         source_income: "coffee",
+  //         Trees: 10,
+  //         Trees_Producing: 5,
+  //         Trees_year_less_10: 3,
+  //         Trees_year_10_20: 4,
+  //         Trees_year_greater_20: 7,
+  //         coffee_plot: 2.5,
+  //         last_season: 2023,
+  //         last_season_production: 1000,
+  //         current_season: 2024,
+  //         current_season_production: 1200,
+  //         nitrogen: "8",
+  //         natural_shade: "12",
+  //         shade_trees: "20",
+  //         other_crops_coffee_farm: "Value",
+  //         other_crops_farm: "Value",
+  //         longitude: 34.123456,
+  //         latitude: -0.123456,
+  //         seedling_last_3_year: 2021,
+  //         received_tree_3_y: 20,
+  //         seedling_last_2_year: 2022,
+  //         received_tree_2_y: 15,
+  //         seedling_last_year: 2023,
+  //         received_tree_l_y: 10,
+  //         rejuvenation_last_year: 2023,
+  //         rejuvenated_l_tree: 7,
+  //         rejuvenation_current_year: 2024,
+  //         rejuvenated_c_tree: 12,
+  //         status: "new",
+  //         Mulching: "Value",
+  //         compost_heap: "Value",
+  //         pruning: "Value",
+  //         trenches: "Value",
+  //         rejuvenation: "Value",
+  //         weed_control: "Value",
+  //         traps: "Value",
+  //         contour_grass: "Value",
+  //         inorganic_waste: "Value",
+  //         leaf_rust: "Value",
+  //         berry_borer: "Value",
+  //         berry_desease: "Value",
+  //         white_stem_borer: "Value",
+  //         scare_mealy_bug: "Value",
+  //         antestia: "Value",
+  //         leaf_miner: "Value",
+  //       },
+  //       {
+  //         created_at: new Date(),
+  //         __kp_house_trees: "DFF32029-405F-BD06-F24C-524157B66281",
+  //         _kf_Staff: "644072CF-DE4C-1F4F-A6E5-373A2AF0D071",
+  //         _kf_User: "83A06EBA-E5D7-0B4A-99E6-9C5674B83A7B",
+  //         full_name: "Nubuhoro Emmanuel",
+  //         _kf_Station: "F9642FF7-694C-4B99-A5B4-0FA1028C4AEC",
+  //         _kf_Supplier: "07463891-1754-4912-B22B-FA01835012D8",
+  //         CW_Name: "Muhura",
+  //         Group_ID: "MM004",
+  //         farmer_ID: "F2315A",
+  //         farmer_name: "Sibomana Theoneste",
+  //         national_ID: "11995856665656",
+  //         Phone: "1234567890",
+  //         Year_Birth: 1980,
+  //         Gender: "Male",
+  //         child_year_1_20: 2,
+  //         child_year_20_30: 3,
+  //         source_income: "coffee",
+  //         Trees: 10,
+  //         Trees_Producing: 5,
+  //         Trees_year_less_10: 3,
+  //         Trees_year_10_20: 4,
+  //         Trees_year_greater_20: 7,
+  //         coffee_plot: 2.5,
+  //         last_season: 2023,
+  //         last_season_production: 1000,
+  //         current_season: 2024,
+  //         current_season_production: 1200,
+  //         nitrogen: "8",
+  //         natural_shade: "12",
+  //         shade_trees: "20",
+  //         other_crops_coffee_farm: "Value",
+  //         other_crops_farm: "Value",
+  //         longitude: 34.123456,
+  //         latitude: -0.123456,
+  //         seedling_last_3_year: 2021,
+  //         received_tree_3_y: 20,
+  //         seedling_last_2_year: 2022,
+  //         received_tree_2_y: 15,
+  //         seedling_last_year: 2023,
+  //         received_tree_l_y: 10,
+  //         rejuvenation_last_year: 2023,
+  //         rejuvenated_l_tree: 7,
+  //         rejuvenation_current_year: 2024,
+  //         rejuvenated_c_tree: 12,
+  //         status: "new",
+  //         Mulching: "Value",
+  //         compost_heap: "Value",
+  //         pruning: "Value",
+  //         trenches: "Value",
+  //         rejuvenation: "Value",
+  //         weed_control: "Value",
+  //         traps: "Value",
+  //         contour_grass: "Value",
+  //         inorganic_waste: "Value",
+  //         leaf_rust: "Value",
+  //         berry_borer: "Value",
+  //         berry_desease: "Value",
+  //         white_stem_borer: "Value",
+  //         scare_mealy_bug: "Value",
+  //         antestia: "Value",
+  //         leaf_miner: "Value",
+  //       }
+  //     );
 
-      console.log("Record inserted successfully:", newRecord);
-    } catch (error) {
-      console.error("Error inserting record:", error);
-    }
-  }
+  //     console.log("Record inserted successfully:", newRecord);
+  //   } catch (error) {
+  //     console.error("Error inserting record:", error);
+  //   }
+  // }
 
-  static async getAllTreeSurveys(req, res) {
+  static async getAllNewTreeSurveys(req, res) {
     try {
       const page = parseInt(req.query.page, 10) || 1;
       const pageSize = parseInt(req.query.pageSize, 10) || 100;
@@ -282,6 +282,7 @@ class HouseholdTreeServey {
 
       const { count, rows: allHouseholdTrees } =
         await Trees_Survey.findAndCountAll({
+          where: { status: "new" },
           offset,
           limit,
         });
@@ -289,13 +290,93 @@ class HouseholdTreeServey {
       if (allHouseholdTrees.length === 0) {
         return res.status(404).json({
           status: "fail",
-          message: "No household trees records found",
+          message: "No recent household trees records found",
         });
       }
 
       return res.status(200).json({
         status: "success",
-        message: "Household trees records retrieved successfully",
+        message: " Recent Household trees records retrieved successfully",
+        data: {
+          totalItems: count,
+          totalPages: Math.ceil(count / pageSize),
+          currentPage: page,
+          household: allHouseholdTrees,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        status: "error",
+        message: "Internal server error",
+      });
+    }
+  }
+  static async getAllApprovedTreeSurveys(req, res) {
+    try {
+      const page = parseInt(req.query.page, 10) || 1;
+      const pageSize = parseInt(req.query.pageSize, 10) || 100;
+
+      const offset = (page - 1) * pageSize;
+      const limit = pageSize;
+
+      const { count, rows: allHouseholdTrees } =
+        await Trees_Survey.findAndCountAll({
+          where: { status: "Approved" },
+          offset,
+          limit,
+        });
+
+      if (allHouseholdTrees.length === 0) {
+        return res.status(404).json({
+          status: "fail",
+          message: "No approved household trees records found",
+        });
+      }
+
+      return res.status(200).json({
+        status: "success",
+        message: " Approved Household trees records retrieved successfully",
+        data: {
+          totalItems: count,
+          totalPages: Math.ceil(count / pageSize),
+          currentPage: page,
+          household: allHouseholdTrees,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        status: "error",
+        message: "Internal server error",
+      });
+    }
+  }
+  static async getAllVerifiedTreeSurveys(req, res) {
+    try {
+      const page = parseInt(req.query.page, 10) || 1;
+      const pageSize = parseInt(req.query.pageSize, 10) || 100;
+
+      const offset = (page - 1) * pageSize;
+      const limit = pageSize;
+
+      const { count, rows: allHouseholdTrees } =
+        await Trees_Survey.findAndCountAll({
+          where: { status: "verified" },
+          offset,
+          limit,
+        });
+
+      if (allHouseholdTrees.length === 0) {
+        return res.status(404).json({
+          status: "fail",
+          message: "No verified household trees records found",
+        });
+      }
+
+      return res.status(200).json({
+        status: "success",
+        message: " Verified Household trees records retrieved successfully",
         data: {
           totalItems: count,
           totalPages: Math.ceil(count / pageSize),
